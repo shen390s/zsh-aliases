42b120720a40dde4ad1018e25bff0cb6648a4835
Rongsong Shen <rshen@shenrs.eu>
2018-03-28 12:17:22 +0800

    use macports

--- zsh-aliases.zsh
+++ zsh-aliases.zsh
@@ -1,7 +1,7 @@
 OS=`uname -s`
 if [ "X$OS" = "XDarwin" ]; then
    alias ec="/Applications/MacPorts/Emacs.app/Contents/MacOS/bin/emacsclient -c -n "
-   alias emacs="open -a /Applications/Emacs.app"
+   alias emacs="open -a /Applications/MacPorts/Emacs.app"
 else	
    alias ec="/usr/local/bin/emacsclient -c -n "
    alias emacs="/usr/local/bin/emacs"
